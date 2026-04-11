f86198f689e9b28feefd840f6e98d9a34de1d014
Mykhailo-Tr <mykhailo.tretiak098@gmail.com>
2026-04-11 12:17:22 +0300

    refactor: separate urls to  urls/teams.py

--- backend/teams/urls.py
+++ backend/teams/urls.py
@@ -1,20 +1,12 @@
 from django.urls import path
 
-from .views import (
-    TeamDetailView,
-    TeamLeaveView,
-    TeamListCreateView,
-)
-
+from .urls.teams import urlpatterns as team_urls
 from .urls.members import urlpatterns as member_urls
 from .urls.invitations import urlpatterns as invitation_urls
 from .urls.join_requests import urlpatterns as join_request_urls
 
 urlpatterns = [
-    path('', TeamListCreateView.as_view(), name='teams'),
-    path('<int:pk>/', TeamDetailView.as_view(), name='team_detail'),
-    path('<int:pk>/leave/', TeamLeaveView.as_view(), name='team_leave'),
-
+    *team_urls,
     *member_urls,
     *invitation_urls,
     *join_request_urls,
